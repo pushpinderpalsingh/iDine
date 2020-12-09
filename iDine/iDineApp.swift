7b69c6abb093c47a0e56c6dfc9cf81fd7ba032cc
Pushpinder Pal Singh <pushpinderpal19@gmail.com>
2020-12-10 01:01:46 +0530

    Initial Commit

---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Pushpinder Pal Singh on 10/12/20.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
